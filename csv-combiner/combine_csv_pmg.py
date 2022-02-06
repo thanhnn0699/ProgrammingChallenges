59147e4340942c2ddd63cfe78ce8d3a8fa879cf1
thanhnn0699 <89351988+thanhnn0699@users.noreply.github.com>
2022-02-05 23:43:11 -0600

    Update combine_csv_pmg.py

--- csv-combiner/combine_csv_pmg.py
+++ csv-combiner/combine_csv_pmg.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import sys
 
+
 def is_command_valid(arg):
     if len(arg) <= 1:
         print('Error: No command input')
@@ -11,32 +12,35 @@ def is_command_valid(arg):
 
     for files in cmdlst:
         if not os.path.exists(files):
-          print('Error: File not found:' + files)
-          return False
+            print('Error: File not found:' + files)
+            return False
         if os.stat(files).st_size == 0:
-          print('Error: The following file is empty:' + files)
-          return False
+            print('Error: The following file is empty:' + files)
+            return False
     return True
+
 
 def combine_csv(arg):
 
-  dfsize = 10 ** 6
-  df_list = []
+    dfsize = 10 ** 6
+    df_list = []
 
-  if is_command_valid(arg):
-    cmdlst = arg[1:]
+    if is_command_valid(arg):
+        cmdlst = arg[1:]
 
-    for files in cmdlst:
-        for df in pd.read_csv(files, chunksize=dfsize):
-            filename = os.path.basename(files)
-            df['filename'] = filename
-            df_list.append(df)
-  header = True
+        for files in cmdlst:
+            for df in pd.read_csv(files, chunksize=dfsize):
+                filename = os.path.basename(files)
+                df['filename'] = filename
+                df_list.append(df)
+        header = True
 
-  for df in df_list:
-      print(df.to_csv(index=False, header=header, line_terminator='\n', chunksize=dfsize))
-      header = False
-  else:
-    return
+        for df in df_list:
+            print(df.to_csv(index=False, header=header,
+                  line_terminator='\n', chunksize=dfsize))
+            header = False
+    else:
+        return
+
 
 combine_csv(sys.argv)
